3b9d6abe83b1a5de828c67a20accfe9039cfa21f
ljx <jiaxin10@mail.ustc.edu.cn>
2018-03-27 17:18:10 +0800

    套用到其他网站上，并修改函数

--- crawler.py
+++ crawler.py
@@ -2,7 +2,8 @@
 
 
 
-#实现以文件夹为单位怕套图，但所有的都是第一张图片
+
+#套用到另一个类似网站上，并修改函数
 
 
 import requests
@@ -11,7 +12,7 @@ from urllib import request
 import os
 import re
 
-def getimg(url,path):
+def getimg(base_url,url,path):
     headers = {
     'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6',
     "Host:": "192.168.1.1",
@@ -25,13 +26,13 @@ def getimg(url,path):
     soup = BeautifulSoup(r.text)
 
     #获取总页码
-    temp = soup.find('div',{"class":"column"}).find('span').text
+    temp = soup.find('div',id='fenye').find('li').text
     total = int(re.findall("\d+",temp)[0])
     #print(soup.prettify())
     #print("total:"+str(total)+"!!!!!!!!")
 
-    t = soup.find('div',id="picbox")
-    request.urlretrieve(t.find('img').get('src').strip(),path+'/'+'1.jpg')
+    t = soup.find('a',href="javascript:dPlayNext();")
+    request.urlretrieve(base_url+t.find('img').get('src').strip(),path+'/'+'1.jpg')
     if total<=1:
         return
     for i in range(2,total+1):
@@ -42,11 +43,11 @@ def getimg(url,path):
         f.write(r2.text)
         f.close()
         soup2 = BeautifulSoup(r2.text)
-        t2 = soup2.find('div',id="picbox")
-        print(t2.find('img').get('src'))
-        request.urlretrieve(t2.find('img').get('src').strip(),path+'/'+str(i)+'.jpg')
+        t2 = soup2.find('a',href="javascript:dPlayNext();")
+        #print(t2.find('img').get('src'))
+        request.urlretrieve(base_url+t2.find('img').get('src').strip(),path+'/'+str(i)+'.jpg')
 
-def getimgfolder(url,path):
+def getimglink(base_url,url,path):
     r = requests.get(url)
     #解决乱码
     r.encoding = 'utf-8'
@@ -56,18 +57,16 @@ def getimgfolder(url,path):
     for img in soup.find_all('a',attrs={"target":"_blank"}):
         #print('!!!!!!!')
         #print(img.get('href'))
-        image_url = 'http://www.99mm.me'+str(img.get('href'))
-        #print('img:'+img.find('img').get('alt')+"!!!!!!")
+        image_url = str(img.get('href'))
         #这样查出来的标签有一个是下面的标题，是多余的
         if img.find('img')== None:
             continue
         name = img.find('img').get('alt')
         #print("name:"+name+"!!!!!!!!!")
         #创建文件夹
-        path = path+name
-        if not os.path.exists(path):
-            os.makedirs(path)
-        getimg(image_url,path)
+        if not os.path.exists(path+name):
+            os.makedirs(path+name)
+        getimg(baseurl,image_url,path+name)
         x += 1
         #request.urlretrieve(image_url.strip(),'./pic/%s.jpg' % x)
 
@@ -75,14 +74,15 @@ def getimgfolder(url,path):
 if __name__ == '__main__':
 
 
-    url = 'http://www.99mm.me'
+    base_url = 'http://www.93xmn.com/'
     #抓几页，每页10个
     page = 1
-    path = './99mm/'
+    path = './94xmn/'
+    url=base_url
     for p in range(1,page+1):
         if(page == 1):
-            getimgfolder(url,path)
+            getimglink(base_url,url,path)
         else:
-            url2 = url + '/hot/mm_4_'+str(page)+'.html'
-            getimgfolder(url2,path)
+            url = base_url + 'xingganmeinv/list_2_'+str(page)+'.html'
+            getimglink(base_url,url,path)
 
