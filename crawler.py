603ddbb4c84f7737fdec3f7a8a96cad2bc0c5995
ljx <jiaxin10@mail.ustc.edu.cn>
2018-03-27 17:18:42 +0800

    将模板重新用于99mm网站

--- crawler.py
+++ crawler.py
@@ -2,8 +2,8 @@
 
 
 
+#将模板重新用于99mm
 
-#套用到另一个类似网站上，并修改函数
 
 
 import requests
@@ -11,6 +11,7 @@ from bs4 import BeautifulSoup
 from urllib import request
 import os
 import re
+from multiprocessing import Process
 
 def getimg(base_url,url,path):
     headers = {
@@ -26,26 +27,26 @@ def getimg(base_url,url,path):
     soup = BeautifulSoup(r.text)
 
     #获取总页码
-    temp = soup.find('div',id='fenye').find('li').text
+    temp = soup.find('div',{"class":"column"}).find('span').text
     total = int(re.findall("\d+",temp)[0])
     #print(soup.prettify())
     #print("total:"+str(total)+"!!!!!!!!")
 
-    t = soup.find('a',href="javascript:dPlayNext();")
-    request.urlretrieve(base_url+t.find('img').get('src').strip(),path+'/'+'1.jpg')
+    t = soup.find('div',id="picbox")
+    request.urlretrieve(t.find('img').get('src').strip(),path+'/'+'1.jpg')
     if total<=1:
         return
     for i in range(2,total+1):
         url2 = url + '?url=' + str(i)
         r2 = requests.get(url2,headers)
         r2.encoding = 'utf-8'
-        f = open('./out.txt','w')
-        f.write(r2.text)
-        f.close()
+        #f = open('./out.txt','w')
+        #f.write(r2.text)
+        #f.close()
         soup2 = BeautifulSoup(r2.text)
-        t2 = soup2.find('a',href="javascript:dPlayNext();")
+        t2 = soup2.find('div',id="picbox")
         #print(t2.find('img').get('src'))
-        request.urlretrieve(base_url+t2.find('img').get('src').strip(),path+'/'+str(i)+'.jpg')
+        request.urlretrieve(t2.find('img').get('src').strip(),path+'/'+str(i)+'.jpg')
 
 def getimglink(base_url,url,path):
     r = requests.get(url)
@@ -57,7 +58,7 @@ def getimglink(base_url,url,path):
     for img in soup.find_all('a',attrs={"target":"_blank"}):
         #print('!!!!!!!')
         #print(img.get('href'))
-        image_url = str(img.get('href'))
+        image_url = base_url + str(img.get('href'))
         #这样查出来的标签有一个是下面的标题，是多余的
         if img.find('img')== None:
             continue
@@ -66,7 +67,7 @@ def getimglink(base_url,url,path):
         #创建文件夹
         if not os.path.exists(path+name):
             os.makedirs(path+name)
-        getimg(baseurl,image_url,path+name)
+        getimg(base_url,image_url,path+name)
         x += 1
         #request.urlretrieve(image_url.strip(),'./pic/%s.jpg' % x)
 
@@ -74,15 +75,15 @@ def getimglink(base_url,url,path):
 if __name__ == '__main__':
 
 
-    base_url = 'http://www.93xmn.com/'
+    base_url = 'http://www.99mm.me/'
     #抓几页，每页10个
     page = 1
-    path = './94xmn/'
+    path = './99mm/'
     url=base_url
     for p in range(1,page+1):
         if(page == 1):
             getimglink(base_url,url,path)
         else:
-            url = base_url + 'xingganmeinv/list_2_'+str(page)+'.html'
+            url = base_url + '/hot/mm_4_'+str(page)+'.html'
             getimglink(base_url,url,path)
 
